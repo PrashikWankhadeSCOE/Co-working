ca04e4dd8d90517381b19f9c8c2027a9cecad994
PrashikWankhadeSCOE <prashikwankhade68@gmail.com>
2024-03-04 12:25:52 +0530

    login authentication done

--- lib/login_page/login_page.dart
+++ lib/login_page/login_page.dart
@@ -35,6 +35,7 @@ class _LoginPageState extends State<LoginPage> {
     } else {}
   }
 
+  final formkey = GlobalKey<FormState>();
   // Snackbar which will show the message that will come if data is posted succesfully
   SnackBar snackbar([String? data]) {
     if (data != null && data.isNotEmpty) {
@@ -107,168 +108,173 @@ class _LoginPageState extends State<LoginPage> {
   bool hide = true;
   @override
   Widget build(BuildContext context) {
-    final formkey = GlobalKey<FormState>();
     return Scaffold(
-      body: Form(
-        key: formkey,
-        child: Padding(
-          padding: const EdgeInsets.all(24),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(
-                height: 89,
-              ),
-              Center(child: Image.asset('assets/df_Icon_small.png')),
-              Center(
-                child: Text(
-                  'Co-working',
+      body: SingleChildScrollView(
+        child: Form(
+          key: formkey,
+          child: Padding(
+            padding: const EdgeInsets.all(24),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  height: 89,
+                ),
+                Center(child: Image.asset('assets/df_Icon_small.png')),
+                Center(
+                  child: Text(
+                    'Co-working',
+                    style: GoogleFonts.poppins(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 24,
+                      color: const Color.fromRGBO(0, 0, 0, 1),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 91,
+                ),
+                Text(
+                  'Mobile number or Email',
                   style: GoogleFonts.poppins(
                     fontWeight: FontWeight.w400,
-                    fontSize: 24,
-                    color: const Color.fromRGBO(0, 0, 0, 1),
+                    fontSize: 16,
+                    color: const Color.fromRGBO(73, 73, 73, 1),
                   ),
                 ),
-              ),
-              const SizedBox(
-                height: 91,
-              ),
-              Text(
-                'Mobile number or Email',
-                style: GoogleFonts.poppins(
-                  fontWeight: FontWeight.w400,
-                  fontSize: 16,
-                  color: const Color.fromRGBO(73, 73, 73, 1),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 10, bottom: 24),
-                child: TextFormField(
-                  controller: emailController,
-                  decoration: InputDecoration(
-                    fillColor: const Color.fromRGBO(218, 218, 218, 1),
-                    filled: true,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter data ';
-                    } else if (!value.contains('@')) {
-                      return 'Email address must contain @';
-                    }
-                    return null;
-                  },
-                ),
-              ),
-              Text(
-                'Password',
-                style: GoogleFonts.poppins(
-                  fontWeight: FontWeight.w400,
-                  fontSize: 16,
-                  color: const Color.fromRGBO(73, 73, 73, 1),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 10, bottom: 24),
-                child: TextFormField(
-                  obscureText: hide,
-                  controller: passwordController,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return "Password cannot be empty";
-                    }
-                    return null;
-                  },
-                  decoration: InputDecoration(
-                    fillColor: const Color.fromRGBO(218, 218, 218, 1),
-                    filled: true,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    suffixIcon: GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          hide = !hide;
-                        });
-                      },
-                      child: Padding(
-                        padding: const EdgeInsets.all(16),
-                        child: Image.asset('assets/mdi_eye.png'),
+                Padding(
+                  padding: const EdgeInsets.only(top: 10, bottom: 24),
+                  child: TextFormField(
+                    controller: emailController,
+                    decoration: InputDecoration(
+                      fillColor: const Color.fromRGBO(218, 218, 218, 1),
+                      filled: true,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
                       ),
                     ),
-                  ),
-                ),
-              ),
-              const Spacer(),
-              Center(
-                child: ElevatedButton(
-                  onPressed: () {
-                    if (formkey.currentState!.validate()) {
-                      setState(() {
-                        fetch();
-                      });
-                      if (message == "Error Occured while Authentication !") {
-                        ScaffoldMessenger.of(context).showSnackBar(snackbar());
-                      } else {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => const HomePage(),
-                          ),
-                        );
-
-                        emailController.clear();
-                        passwordController.clear();
-
-                        ScaffoldMessenger.of(context).showSnackBar(snackbar());
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter data ';
+                      } else if (!value.contains('@')) {
+                        return 'Email address must contain @';
                       }
-                    }
-                  },
-                  style:
-                      ElevatedButton.styleFrom(fixedSize: const Size(312, 56)),
-                  child: Text(
-                    'Log in',
-                    style: GoogleFonts.poppins(
-                      fontWeight: FontWeight.w700,
-                      fontSize: 16,
+                      return null;
+                    },
+                  ),
+                ),
+                Text(
+                  'Password',
+                  style: GoogleFonts.poppins(
+                    fontWeight: FontWeight.w400,
+                    fontSize: 16,
+                    color: const Color.fromRGBO(73, 73, 73, 1),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 10, bottom: 24),
+                  child: TextFormField(
+                    obscureText: hide,
+                    controller: passwordController,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Password cannot be empty";
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(
+                      fillColor: const Color.fromRGBO(218, 218, 218, 1),
+                      filled: true,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      suffixIcon: GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            hide = !hide;
+                          });
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.all(16),
+                          child: Image.asset('assets/mdi_eye.png'),
+                        ),
+                      ),
                     ),
                   ),
                 ),
-              ),
-              Center(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'New user? ',
+                // const Spacer(),
+                SizedBox(height: MediaQuery.of(context).size.height * 0.30),
+
+                Center(
+                  child: ElevatedButton(
+                    onPressed: () async {
+                      if (formkey.currentState!.validate()) {
+                        // setState(() {
+                        await fetch();
+                        // });
+                        if (message == "Error Occured while Authentication !") {
+                          ScaffoldMessenger.of(context)
+                              .showSnackBar(snackbar());
+                        } else {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const HomePage(),
+                            ),
+                          );
+
+                          emailController.clear();
+                          passwordController.clear();
+
+                          ScaffoldMessenger.of(context)
+                              .showSnackBar(snackbar());
+                        }
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                        fixedSize: const Size(312, 56)),
+                    child: Text(
+                      'Log in',
                       style: GoogleFonts.poppins(
-                          fontSize: 14,
-                          fontWeight: FontWeight.w400,
-                          color: const Color.fromRGBO(98, 98, 98, 1)),
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const RegistrationPage()),
-                        );
-                      },
-                      child: Text(
-                        'Create an account',
-                        style: GoogleFonts.poppins(
-                            decoration: TextDecoration.underline,
-                            fontSize: 16,
-                            fontWeight: FontWeight.w500,
-                            color: const Color.fromRGBO(42, 29, 139, 1)),
+                        fontWeight: FontWeight.w700,
+                        fontSize: 16,
                       ),
-                    )
-                  ],
+                    ),
+                  ),
                 ),
-              ),
-            ],
+                Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'New user? ',
+                        style: GoogleFonts.poppins(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400,
+                            color: const Color.fromRGBO(98, 98, 98, 1)),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const RegistrationPage()),
+                          );
+                        },
+                        child: Text(
+                          'Create an account',
+                          style: GoogleFonts.poppins(
+                              decoration: TextDecoration.underline,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500,
+                              color: const Color.fromRGBO(42, 29, 139, 1)),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/login_page/registration_page.dart
+++ lib/login_page/registration_page.dart
@@ -14,6 +14,7 @@ class RegistrationPage extends StatefulWidget {
 }
 
 class _RegistrationPageState extends State<RegistrationPage> {
+  final formkey = GlobalKey<FormState>();
   String message = 'Registration Failed !';
 
   TextEditingController emailController = TextEditingController();
@@ -39,189 +40,185 @@ class _RegistrationPageState extends State<RegistrationPage> {
 
   @override
   Widget build(BuildContext context) {
-    final formkey = GlobalKey<FormState>();
     return Scaffold(
-      body: Form(
-        key: formkey,
-        child: Padding(
-          padding: const EdgeInsets.all(24),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const SizedBox(
-                    height: 90,
-                  ),
-                  Text(
-                    'Create an Account',
-                    style: GoogleFonts.poppins(
-                      fontWeight: FontWeight.w400,
-                      fontSize: 24,
-                      color: const Color.fromRGBO(0, 0, 0, 1),
+      body: SingleChildScrollView(
+        child: Form(
+          key: formkey,
+          child: Padding(
+            padding: const EdgeInsets.all(24),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(
+                      height: 90,
                     ),
-                  ),
-                  const SizedBox(
-                    height: 58,
-                  ),
-                  Text(
-                    'Full name',
-                    style: GoogleFonts.poppins(
-                      fontWeight: FontWeight.w400,
-                      fontSize: 16,
-                      color: const Color.fromRGBO(73, 73, 73, 1),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 10, bottom: 24),
-                    child: TextFormField(
-                      controller: nameController,
-                      decoration: InputDecoration(
-                        fillColor: const Color.fromRGBO(218, 218, 218, 1),
-                        filled: true,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
+                    Text(
+                      'Create an Account',
+                      style: GoogleFonts.poppins(
+                        fontWeight: FontWeight.w400,
+                        fontSize: 24,
+                        color: const Color.fromRGBO(0, 0, 0, 1),
                       ),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter some text';
-                        }
-                        return null;
-                      },
                     ),
-                  ),
-                  Text(
-                    'Mobile number',
-                    style: GoogleFonts.poppins(
-                      fontWeight: FontWeight.w400,
-                      fontSize: 16,
-                      color: const Color.fromRGBO(73, 73, 73, 1),
+                    const SizedBox(
+                      height: 58,
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 10, bottom: 24),
-                    child: TextFormField(
-                      validator: (value) {
-                        bool isNumeric(String str) {
-                          try {
-                            double.parse(str);
-                            return true;
-                          } on FormatException {
-                            return false;
+                    Text(
+                      'Full name',
+                      style: GoogleFonts.poppins(
+                        fontWeight: FontWeight.w400,
+                        fontSize: 16,
+                        color: const Color.fromRGBO(73, 73, 73, 1),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10, bottom: 24),
+                      child: TextFormField(
+                        controller: nameController,
+                        decoration: InputDecoration(
+                          fillColor: const Color.fromRGBO(218, 218, 218, 1),
+                          filled: true,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter some text';
                           }
-                        }
+                          return null;
+                        },
+                      ),
+                    ),
+                    Text(
+                      'Mobile number',
+                      style: GoogleFonts.poppins(
+                        fontWeight: FontWeight.w400,
+                        fontSize: 16,
+                        color: const Color.fromRGBO(73, 73, 73, 1),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10, bottom: 24),
+                      child: TextFormField(
+                        validator: (value) {
+                          bool isNumeric(String str) {
+                            try {
+                              double.parse(str);
+                              return true;
+                            } on FormatException {
+                              return false;
+                            }
+                          }
 
-                        if (value!.length != 10) {
-                          return 'Mobile no should be 10 digits';
-                        } else if (!isNumeric(value)) {
-                          return 'Should contain Digits only';
-                        }
-                        return null;
-                      },
-                      controller: mobileController,
-                      decoration: InputDecoration(
-                        filled: true,
-                        fillColor: const Color.fromRGBO(218, 218, 218, 1),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
+                          if (value!.length != 10) {
+                            return 'Mobile no should be 10 digits';
+                          } else if (!isNumeric(value)) {
+                            return 'Should contain Digits only';
+                          }
+                          return null;
+                        },
+                        controller: mobileController,
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: const Color.fromRGBO(218, 218, 218, 1),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  Text(
-                    'Email ID',
-                    style: GoogleFonts.poppins(
-                      fontWeight: FontWeight.w400,
-                      fontSize: 16,
-                      color: const Color.fromRGBO(73, 73, 73, 1),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 10, bottom: 24),
-                    child: TextFormField(
-                      controller: emailController,
-                      decoration: InputDecoration(
-                        fillColor: const Color.fromRGBO(218, 218, 218, 1),
-                        filled: true,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
+                    Text(
+                      'Email ID',
+                      style: GoogleFonts.poppins(
+                        fontWeight: FontWeight.w400,
+                        fontSize: 16,
+                        color: const Color.fromRGBO(73, 73, 73, 1),
                       ),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please Enter the Data';
-                        } else if (!value.contains('@')) {
-                          return 'Email should contain @ Symbol';
-                        }
-                        return null;
-                      },
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10, bottom: 24),
+                      child: TextFormField(
+                        controller: emailController,
+                        decoration: InputDecoration(
+                          fillColor: const Color.fromRGBO(218, 218, 218, 1),
+                          filled: true,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please Enter the Data';
+                          } else if (!value.contains('@')) {
+                            return 'Email should contain @ Symbol';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: MediaQuery.of(context).size.height * 0.25),
+                Center(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (formkey.currentState!.validate()) {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (contex) => const HomePage(),
+                          ),
+                        );
+                      } else {}
+                    },
+                    style: ElevatedButton.styleFrom(
+                        fixedSize: const Size(312, 56)),
+                    child: Text(
+                      'Create an account',
+                      style: GoogleFonts.poppins(
+                        fontWeight: FontWeight.w700,
+                        fontSize: 16,
+                      ),
                     ),
                   ),
-                ],
-              ),
-              Column(
-                children: [
-                  Center(
-                    child: ElevatedButton(
-                      onPressed: () {
-                        if (formkey.currentState!.validate()) {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (contex) => const HomePage(),
-                            ),
-                          );
-                        } else {}
-                      },
-                      style: ElevatedButton.styleFrom(
-                          fixedSize: const Size(312, 56)),
-                      child: Text(
-                        'Create an account',
+                ),
+                Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Existing user? ',
                         style: GoogleFonts.poppins(
-                          fontWeight: FontWeight.w700,
-                          fontSize: 16,
-                        ),
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400,
+                            color: const Color.fromRGBO(98, 98, 98, 1)),
                       ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 14,
-                  ),
-                  Center(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Existing user? ',
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const LoginPage()));
+                        },
+                        child: Text(
+                          'Log In',
                           style: GoogleFonts.poppins(
+                              decoration: TextDecoration.underline,
                               fontSize: 14,
                               fontWeight: FontWeight.w400,
-                              color: const Color.fromRGBO(98, 98, 98, 1)),
+                              color: const Color.fromRGBO(42, 29, 139, 1)),
                         ),
-                        GestureDetector(
-                          onTap: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => const LoginPage()));
-                          },
-                          child: Text(
-                            'Log In',
-                            style: GoogleFonts.poppins(
-                                decoration: TextDecoration.underline,
-                                fontSize: 14,
-                                fontWeight: FontWeight.w400,
-                                color: const Color.fromRGBO(42, 29, 139, 1)),
-                          ),
-                        )
-                      ],
-                    ),
+                      )
+                    ],
                   ),
-                ],
-              ),
-            ],
+                )
+              ],
+            ),
           ),
         ),
       ),
