1dceb9464dc7d04c32626bc922416eb4f2381718
PrashikWankhadeSCOE <prashikwankhade68@gmail.com>
2024-03-04 12:24:18 +0530

    Certificate lisence error solved

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
+import 'dart:io';
+
 import 'package:coworking/login_page/login_page.dart';
 import 'package:flutter/material.dart';
 
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
+}
+
 void main() {
+  HttpOverrides.global = MyHttpOverrides();
   runApp(const MainApp());
 }
 
